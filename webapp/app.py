9bcc28c0c1a8b752dcdc2b9bb7d95f447f0afc97
nutcasee <nexustivi@gmail.com>
2021-02-19 17:28:20 +0700

    add .github, heroku.yml...no Procfile...deploy via github actions...huhuhu

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world! fukc you, circleci, from github actions'
+    return 'Hello world! fukc you, circleci, from github actions && /' 
+        + 'no, no, no...fukc github actions too'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
